ce46a4f71bf536ba3785cf0ce86930f8d7aa54c8
Tristin Nguyen <nguye501@msu.edu>
2022-08-31 20:01:26 -0400

    added form to classroom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Container style={{ paddingTop: "50px" }}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/classroom" element={<classRoom/>} />
+          <Route path="/Classroom" element={<classRoom/>} />
         </Routes>
       </Container>
       </Router>
