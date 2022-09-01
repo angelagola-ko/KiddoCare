2059682e54122528701255225165cd1f757c7668
Angela Gola <angelagola.ko@gmail.com>
2022-09-01 11:45:08 -0400

    Added footer

---
+++ my-app/src/components/footer.js
@@ -0,0 +1,18 @@
+const Footer = (props) => {
+
+    const styling = {
+        textAlign: "center",
+        color: "white",
+        opacity: "25%",
+        fontSize: "12px"
+      }
+    return (
+      <footer>
+        <p style={styling}>
+        &copy; 2022 All Rights Reserved
+        </p>
+      </footer>
+    )
+  }
+  
+  export default Footer
