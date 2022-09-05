52ae512470febd0941276fa5c1ba35c1d6fe3bd4
Brandon Benson <brandonbenson09@gmail.com>
2022-09-05 16:18:04 -0400

    started layout for profile page

---
+++ my-app/src/pages/Profile.js
@@ -0,0 +1,13 @@
+import "./Profile.css";
+
+const Profile = (props) => {
+  return (
+    <main>
+      <div class="">
+        <img src="" alt="" class="image--cover" />
+      </div>
+    </main>
+  );
+};
+
+export default Profile;
