f69763070097a1a17b6465b72e7934e49a9330b7
Brandon Benson <brandonbenson09@gmail.com>
2022-09-05 20:15:22 -0400

    minor tweeks

--- my-app/src/pages/Classroom.js
+++ my-app/src/pages/Classroom.js
@@ -1,43 +1,36 @@
-import {Container, Form, Button} from "react-bootstrap"
-import {useState} from "react"
-
+import { Container, Form, Button } from "react-bootstrap";
+import { useState } from "react";
 
 const ClassRoom = (props) => {
-    const [ActivityAuto] = useState({title:"Finger Painting", time: "10:30"})
-    let today = new Date();
-    const dd = String(today.getDate()).padStart(2, '0');
-    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    const yyyy = today.getFullYear();
-    today = mm + '/' + dd + '/' + yyyy;
-// add functionality to button to "today's activities here". Normal JS//
-    return (
-        <div className= "home">
-            <h1 className= "homeTitle text-white">
-                Daycare Room 187
-                Overview {today}
-            </h1>
-        
-        <Container>
-            <h2 className="text-white">Today's Activities</h2>
-            {/* <Form.Label className="text-white">Today's Activities</Form.Label> */}
-            <Form.Control
-                type= "title"
-                name= "title"
-                value= {ActivityAuto.title}
-                />
-        </Container>
-        <br></br>
-        <br></br>
-        <br></br>
-        <Container>
-            <h2 className="text-white">Kids of Classroom 187</h2>
-            {/* insert database with students here */}
-        </Container>
+  const [ActivityAuto] = useState({ title: "Finger Painting", time: "10:30" });
+  let today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  const yyyy = today.getFullYear();
+  today = mm + "/" + dd + "/" + yyyy;
+  // add functionality to button to "today's activities here". Normal JS//
+  return (
+    <div className="home">
+      <h2 className="homeTitle text-white">
+        Daycare Room 187 Overview {today}
+      </h2>
 
+      <Container>
+        <h4 className="text-white">Today's Activities</h4>
+        {/* <Form.Label className="text-white">Today's Activities</Form.Label> */}
+        <Form.Control type="title" name="title" value={ActivityAuto.title} />
+      </Container>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Container>
+        <h4 className="text-white">Kids of Classroom 187</h4>
+        {/* insert database with students here */}
+      </Container>
 
-        <Button variant="primary">Admin Add Activities</Button>
-        </div> 
-    );
-}
+      <Button variant="primary">Admin Add Activities</Button>
+    </div>
+  );
+};
 
-export default ClassRoom
+export default ClassRoom;
--- my-app/src/pages/Login.js
+++ my-app/src/pages/Login.js
@@ -30,8 +30,8 @@ const Login = (props) => {
   //     }
   //  };
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="card col-12 col-md-6">
+    <main className="flex-row justify-center">
+      <div className="col-12 col-md-6">
         <div className="card">
           <h4 className="card-header">Sign Up</h4>
           <div className="card-body">
