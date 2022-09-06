ca253e669f55e7729e559758ba40845b3c14e298
Brandon Benson <brandonbenson09@gmail.com>
2022-09-06 16:00:05 -0400

    asd

--- my-app/src/pages/Profile.js
+++ my-app/src/pages/Profile.js
@@ -1,18 +1,19 @@
 const Profile = (props) => {
   return (
     <main className="flex-row justify-center mb-4">
-      <body className="col-12">
-        <img src="" alt="" class="image--cover" />
+      <div className="col-12">
+        {/* <img src="" alt="" class="image--cover" /> */}
         <div className="card">
-          <h4 className="card-header ">Profile Page</h4>
+          <h4 className="card-header">Profile Page</h4>
           <div className="card-body text-white">
-            <label className="p-2" for="switch">
+            <input className="text-white" />
+            <input className="text-white" />
+            <label className="p-2" for="switch" type="checkbox" id="switch">
               Notifications
             </label>
-            <input type="checkbox" id="switch" />
           </div>
         </div>
-      </body>
+      </div>
     </main>
   );
 };
