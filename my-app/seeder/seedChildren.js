03b6463917019773c117bd55e805aee6d3e2f5e6
Tristin Nguyen <nguye501@msu.edu>
2022-09-07 12:09:29 -0400

    fixed bugs

--- my-app/seeder/seedChildren.js
+++ my-app/seeder/seedChildren.js
@@ -13,24 +13,24 @@ const childrenData = [
     })
 ]
 //connect mongoose
-mongoose
-  .connect(String(dev.db), { useNewUrlParser: true })
-  .catch(err => {
-    console.log(err.stack);
-    process.exit(1);
-  })
-  .then(() => {
-    console.log("connected to db in development environment");
-  });
+// mongoose
+//   .connect(String(dev.db), { useNewUrlParser: true })
+//   .catch(err => {
+//     console.log(err.stack);
+//     process.exit(1);
+//   })
+//   .then(() => {
+//     console.log("connected to db in development environment");
+//   });
 //save your data. this is an async operation
 //after you make sure you seeded all the products, disconnect automatically
-products.map(async (p, index) => {
-  await p.save((err, result) => {
-    if (index === products.length - 1) {
-      console.log("DONE!");
-      mongoose.disconnect();
-    }
-  });
-});
+// products.map(async (p, index) => {
+//   await p.save((err, result) => {
+//     if (index === products.length - 1) {
+//       console.log("DONE!");
+//       mongoose.disconnect();
+//     }
+//   });
+// });
 module.exports = childrenData
 //run seed with 'node seedChildren.js' once //
