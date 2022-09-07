32ef3efa393614bcae10580fee138cd2c0991cdf
Brandon Benson <brandonbenson09@gmail.com>
2022-09-06 20:13:48 -0400

    oops

--- my-app/src/pages/Profile.js
+++ my-app/src/pages/Profile.js
@@ -1,5 +1,21 @@
 const Profile = (props) => {
-  console.log(Profile);
+  return (
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12">
+        {/* <img src="" alt="" class="image--cover" /> */}
+        <div className="card">
+          <h4 className="card-header">Profile Page</h4>
+          <div className="card-body text-white">
+            <input className="text-white" />
+            <input className="text-white" />
+            <label className="p-2" for="switch" type="checkbox" id="switch">
+              Notifications
+            </label>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Profile;
